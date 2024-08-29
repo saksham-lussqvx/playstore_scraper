a83c5d19239e3d407fa05fbf574de5bb4752d8be
saksham-lussqvx <saksham.lussqvx.github@gmail.com>
2024-08-29 23:02:37 +0530

    Cleaning of csv file

--- scraper.py
+++ scraper.py
@@ -4,3 +4,21 @@ import time
 
 # define the base url
 base_url = "https://play.google.com/store/apps/details?id="
+
+# open the file containing the list of app ids
+file_name = "apk_list_full.csv"
+
+with open(file_name, "r") as f:
+    app_ids = f.read().split("\n")
+
+final_ids = []
+
+for app_id in app_ids:
+    # remove double quotation marks and save the app id
+    app_id = app_id.replace('"', "")
+    final_ids.append(app_id)
+
+# rewrite the file with the cleaned app ids
+with open(file_name, "w") as f:
+    for app_id in final_ids:
+        f.write(app_id + "\n")
