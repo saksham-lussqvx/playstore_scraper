629e5af6d298911bac11601aef65f5b59234d04a
saksham-lussqvx <saksham.lussqvx.github@gmail.com>
2024-08-30 00:47:25 +0530

    scraper v2 done

--- scraper.py
+++ scraper.py
@@ -1,7 +1,8 @@
 from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup
-
+import pandas
+import os
 
 # define the base url
 base_url = "https://play.google.com/store/apps/details?id="
@@ -9,6 +10,9 @@ base_url = "https://play.google.com/store/apps/details?id="
 page = None
 # open the file containing the list of app ids
 file_name = "apk_list_full.csv"
+# filename for output file
+filename = "final_data.xlsx"
+
 
 with open(file_name, "r") as f:
     app_ids = f.read().split("\n")
@@ -18,8 +22,8 @@ def fetch_app_info(app_id:str) -> dict:
     global page
     url = base_url + app_id
     page.goto(url, wait_until="domcontentloaded")
-    time.sleep(2)
     app_info = {}
+    app_info["app_id"] = app_id
     #<div id="error-section" class="uaxL4e">We're sorry, the requested URL was not found on this server.</div>
     # first check if the app is available, if not then return None
     if page.query_selector("#error-section"):
@@ -30,47 +34,96 @@ def fetch_app_info(app_id:str) -> dict:
     soup = BeautifulSoup(html, "html.parser")
     # get the title
     #<h1 class="Fd93Bb F5UCq p5VxAd" itemprop="name">QuickBooks Online Accounting</h1>
-    title = soup.find("h1", {"class": "Fd93Bb F5UCq p5VxAd"}).text
+    title = soup.find("h1", {"itemprop": "name"}).text
     app_info["title"] = title
     # get the category
-    #<a class="WpHeLc VfPpkd-mRLv6 VfPpkd-RLmnJb" href="/store/apps/category/BUSINESS" aria-label="Business" jsname="hSRGPd"></a>
-    category = soup.find_all("a", {"class": "WpHeLc VfPpkd-mRLv6 VfPpkd-RLmnJb"})[-1].text
-    app_info["category"] = category
+    #<span jsname="V67aGc" class="VfPpkd-vQzf8d" aria-hidden="true">Books &amp; Reference</span>
+    try:
+        #<div class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-INsAgc VfPpkd-LgbsSe-OWXEXe-dgl2Hf Rj2Mlf OLiIxf PDpWxe P62QJc LQeN7 LMoCf" jscontroller="nKuFpb" jsaction="click:cOuCgd; mousedown:UX7yZ; mouseup:lbsD7e; mouseenter:tfO1Yc; mouseleave:JywGue; touchstart:p6p2H; touchmove:FwuNnf; touchend:yfqBxc; touchcancel:JMtRjd; focus:AHmuwe; blur:O22p3e; contextmenu:mg9Pef;mlnRJb:fLiPzd;" data-idom-class="Rj2Mlf OLiIxf PDpWxe P62QJc LQeN7 LMoCf" itemprop="genre"><div class="VfPpkd-Jh9lGc"></div><span jsname="V67aGc" class="VfPpkd-vQzf8d" aria-hidden="true">Books &amp; Reference</span><a class="WpHeLc VfPpkd-mRLv6 VfPpkd-RLmnJb" href="/store/apps/category/BOOKS_AND_REFERENCE" aria-label="Books &amp; Reference" jsname="hSRGPd"></a><div class="VfPpkd-J1Ukfc-LhBDec"></div></div>
+        main_div = soup.find("div", {"itemprop": "genre"})
+        category = main_div.find_all("span", {"jsname": "V67aGc"})[-1].text
+        app_info["category"] = category
+    except:
+        app_info["category"] = ""
     # get the rating
     #<div class="wVqUob"><div class="ClM7O"><div itemprop="starRating"><div class="TT9eCd" aria-label="Rated 4.7 stars out of five stars">4.7<i class="google-material-icons ERwvGb" aria-hidden="true">star</i></div></div></div><div class="g1rdde">33K reviews</div></div>
-    rating = soup.find("div", {"itemprop": "starRating"}).text
-    app_info["rating"] = rating
-    # get the review count
-    review_count = soup.find("div", {"class": "g1rdde"}).text.split()[0]
-    app_info["review_count"] = review_count
+    try:
+        rating = soup.find("div", {"itemprop": "starRating"}).text
+        app_info["rating"] = rating
+    except:
+        app_info["rating"] = ""
+    try:
+        # get the review count
+        review_count = soup.find("div", {"class": "g1rdde"}).text.split()[0]
+        # if there is no number in review count then set it to empty string
+        # there could be alphabets in the review count
+        x = False
+        for i in review_count:
+            if i.isdigit():
+                x = True
+                break
+        if not x:
+            review_count = ""
+        app_info["review_count"] = review_count
+    except:
+        app_info["review_count"] = ""
     # get the download count
-    #<div class="wVqUob"><div class="ClM7O">1M+</div><div class="g1rdde">Downloads</div></div>
-    download_count = soup.find("div", {"class": "ClM7O"}).text
-    app_info["download_count"] = download_count
+    try:
+        #<div class="wVqUob"><div class="ClM7O">1M+</div><div class="g1rdde">Downloads</div></div>
+        main_tag = soup.find_all("div", {"class": "wVqUob"})
+        download_count = ""
+        for tag in main_tag:
+            if "Downloads" in tag.text:
+                download_count = tag.find("div", {"class": "ClM7O"}).text
+                break
+        app_info["download_count"] = download_count
+    except:
+        app_info["download_count"] = ""
     # get the icon link
-    #<div class="l8YSdd"><img src="https://play-lh.googleusercontent.com/VX1WlbpWZDSmPLhsBwJ8H6GYnBQD2MQiAr7FgC3KpLhN8NeVx7t_oD9MxSymGiryDg=s48-rw" srcset="https://play-lh.googleusercontent.com/VX1WlbpWZDSmPLhsBwJ8H6GYnBQD2MQiAr7FgC3KpLhN8NeVx7t_oD9MxSymGiryDg=s96-rw 2x" class="T75of QhHVZd" aria-hidden="true" alt="Icon image" itemprop="image" data-iml="24310473.6"><div class="w7Iutd"><div class="wVqUob"><div class="ClM7O"><div itemprop="starRating"><div class="TT9eCd" aria-label="Rated 3.3 stars out of five stars">3.3<i class="google-material-icons ERwvGb" aria-hidden="true">star</i></div></div></div><div class="g1rdde">13.8K reviews</div></div><div class="wVqUob"><div class="ClM7O">1M+</div><div class="g1rdde">Downloads</div></div><div class="wVqUob"><div class="ClM7O"><img src="https://play-lh.googleusercontent.com/EbEX3AN4FC4pu3lsElAHCiksluOVU8OgkgtWC43-wmm_aHVq2D65FmEM97bPexilUAvlAY5_4ARH8Tb3RxQ=w48-h16-rw" srcset="https://play-lh.googleusercontent.com/EbEX3AN4FC4pu3lsElAHCiksluOVU8OgkgtWC43-wmm_aHVq2D65FmEM97bPexilUAvlAY5_4ARH8Tb3RxQ=w96-h32-rw 2x" class="T75of xGa6dd" alt="Content rating" itemprop="image" data-iml="24310447.800000004"></div><div class="g1rdde"><span itemprop="contentRating"><span>Rated for 3+</span></span><div jsaction="click:CnOdef" class="MKV5ee" role="button" tabindex="0" jscontroller="kJXwXb" aria-label="More info about this content rating"><i class="google-material-icons oUaal" aria-hidden="true">info</i></div></div></div></div></div>
-    icon_link = soup.find("img", {"itemprop": "image"}).get("src")
-    app_info["icon_link"] = icon_link
-    # get the developer email
-    #<a class="Si6A0c RrSxVb" target="_blank" href="mailto:support+mobile@datacamp.com" aria-label="Support email mailto:support+mobile@datacamp.com will open in a new window or tab."><i class="google-material-icons j25Vu" aria-hidden="true">email</i><div class="pZ8Djf"><div class="xFVDSb">Support email</div><div class="pSEeg">support+mobile@datacamp.com</div></div></a>
-    developer_email = soup.find("a", {"class": "Si6A0c RrSxVb"}).text
-    app_info["developer_email"] = developer_email
+    try:
+        #<div class="l8YSdd"><img src="https://play-lh.googleusercontent.com/VX1WlbpWZDSmPLhsBwJ8H6GYnBQD2MQiAr7FgC3KpLhN8NeVx7t_oD9MxSymGiryDg=s48-rw" srcset="https://play-lh.googleusercontent.com/VX1WlbpWZDSmPLhsBwJ8H6GYnBQD2MQiAr7FgC3KpLhN8NeVx7t_oD9MxSymGiryDg=s96-rw 2x" class="T75of QhHVZd" aria-hidden="true" alt="Icon image" itemprop="image" data-iml="24310473.6"><div class="w7Iutd"><div class="wVqUob"><div class="ClM7O"><div itemprop="starRating"><div class="TT9eCd" aria-label="Rated 3.3 stars out of five stars">3.3<i class="google-material-icons ERwvGb" aria-hidden="true">star</i></div></div></div><div class="g1rdde">13.8K reviews</div></div><div class="wVqUob"><div class="ClM7O">1M+</div><div class="g1rdde">Downloads</div></div><div class="wVqUob"><div class="ClM7O"><img src="https://play-lh.googleusercontent.com/EbEX3AN4FC4pu3lsElAHCiksluOVU8OgkgtWC43-wmm_aHVq2D65FmEM97bPexilUAvlAY5_4ARH8Tb3RxQ=w48-h16-rw" srcset="https://play-lh.googleusercontent.com/EbEX3AN4FC4pu3lsElAHCiksluOVU8OgkgtWC43-wmm_aHVq2D65FmEM97bPexilUAvlAY5_4ARH8Tb3RxQ=w96-h32-rw 2x" class="T75of xGa6dd" alt="Content rating" itemprop="image" data-iml="24310447.800000004"></div><div class="g1rdde"><span itemprop="contentRating"><span>Rated for 3+</span></span><div jsaction="click:CnOdef" class="MKV5ee" role="button" tabindex="0" jscontroller="kJXwXb" aria-label="More info about this content rating"><i class="google-material-icons oUaal" aria-hidden="true">info</i></div></div></div></div></div>
+        icon_link = soup.find("img", {"itemprop": "image"}).get("src")
+        app_info["icon_link"] = icon_link
+    except:
+        app_info["icon_link"] = ""
+    try:
+        # get the developer email
+        #<a class="Si6A0c RrSxVb" target="_blank" href="mailto:support+mobile@datacamp.com" aria-label="Support email mailto:support+mobile@datacamp.com will open in a new window or tab."><i class="google-material-icons j25Vu" aria-hidden="true">email</i><div class="pZ8Djf"><div class="xFVDSb">Support email</div><div class="pSEeg">support+mobile@datacamp.com</div></div></a>
+        emails = soup.find_all("a", {"class": "Si6A0c RrSxVb", "target": "_blank"})
+        developer_email = ""
+        for email in emails:
+            if "mailto" in email.get("href"):
+                developer_email = email.get("href").split(":")[-1]
+                break
+        app_info["developer_email"] = developer_email
+    except:
+        app_info["developer_email"] = ""
     # get the short description
-    desc_short = soup.find("meta", {"itemprop": "description"}).get("content")
-    app_info["desc_short"] = desc_short
-    # get the long description
-    desc_long = soup.find("div", {"data-g-id": "description"}).text
-    app_info["desc_long"] = desc_long
-    #convert desc_long to a raw string to preserve the new lines and commas too
-    desc_long = repr(desc_long)
-    # do this for desc_short too
-    desc_short = repr(desc_short)
+    try:
+        desc_short = soup.find("meta", {"itemprop": "description"}).get("content")
+        app_info["desc_short"] = desc_short.replace("|", " ")
+    except:
+        app_info["desc_short"] = ""
+    try:
+        # get the long description
+        desc_long = soup.find("div", {"data-g-id": "description"}).text
+        app_info["desc_long"] = desc_long.replace("|", " ")
+    except:
+        app_info["desc_long"] = ""
+
     # return the app info
     return app_info
 
 
 
 if __name__ == "__main__":
+    # check if there is a file with the name final_data.xlsx
+    try:
+        if not os.path.exists(filename):
+            df = pandas.DataFrame(columns=["app_id","title", "category", "rating", "review_count", "download_count", "icon_link", "developer_email", "desc_short", "desc_long"])
+            df.to_excel(filename, index=False)
+    except:
+        pass
     p = sync_playwright().start()
     browser = p.chromium.launch(headless=False)
     context = browser.new_context()
@@ -84,14 +137,21 @@ if __name__ == "__main__":
         app_info = fetch_app_info(app_id)
         if app_info:
             print(app_info)
-            with open("final_data.csv", "a", encoding="utf-8") as f:
-                f.write(f"{app_info['title']},{app_info['category']},{app_info['rating']},{app_info['review_count']},{app_info['download_count']},{app_info['icon_link']},{app_info['desc_short']},{app_info['desc_long']},{app_info['developer_email']}\n")
+            # save it in the excel file
+            df = pandas.read_excel(filename)
+            df_new = pandas.DataFrame([app_info])
+            df = pandas.concat([df, df_new], ignore_index=True)
+            df.to_excel(filename, index=False)
+            # add the app id to the processed app ids file
             with open("processed_app_ids.txt", "a") as f:
                 f.write(f"{app_id}\n")
         # if app info is None then just add the app id to the file
         else:
-            with open("final_data.csv", "a", encoding="utf-8") as f:
-                f.write(f"{app_id},,,,,,,,\n")
+            df = pandas.read_excel(filename)
+            df_new = pandas.DataFrame([{"app_id":app_id,"title": "", "category": "", "rating": "", "review_count": "", "download_count": "", "icon_link": "", "developer_email": "", "desc_short": "", "desc_long": ""}])
+            df = pandas.concat([df, df_new], ignore_index=True)
+            df.to_excel(filename, index=False)
+            # add the app id to the processed app ids file
             with open("processed_app_ids.txt", "a") as f:
                 f.write(f"{app_id}\n")
     browser.close()
